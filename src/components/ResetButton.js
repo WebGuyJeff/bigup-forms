e619f1a9856a70b289b1fc2a382f656f59f07eb8
BigupJeff <me@jeffersonreal.uk>
2025-12-31 11:40:50 +0000

    fix style inconsistencies

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -2,7 +2,7 @@ const ResetButton = () => {
 	return (
 		<div className='bigupForms__buttonWrap is-style-outline'>
 			<button
-				className='bigupForms__button bigupForms__button-reset wp-element-button wp-block-button__link'
+				className='bigupForms__button bigupForms__button-reset wp-element-button'
 				disabled
 			>
 				<span>
